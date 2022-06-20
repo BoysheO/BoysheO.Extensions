c1483d4dad4f68a6e22fce25f65afc9d2d724c5d
ADMINOR-JNBBMUB\admin <obx100@163.com>
2022-06-20 14:55:13 +0800

    ToSummaryText1

--- src/BoysheO.Extensions/Extensions/TimeSpanExtensions.cs
+++ src/BoysheO.Extensions/Extensions/TimeSpanExtensions.cs
@@ -32,5 +32,20 @@ namespace BoysheO.Extensions
         {
             return value.Max(TimeSpan.Zero);
         }
+
+        /// <summary>
+        /// 描述时间的大概长度
+        /// ex:56d13h12m5s=>56d
+        /// ex:0d13h12m5s=>13h
+        /// ex:0d0h0m5s=>5s
+        /// todo:make it low gc
+        /// </summary>
+        public static string ToSummaryText1(this TimeSpan value,string day,string hour,string minute,string second)
+        {
+            if (value.Days > 0) return value.Days + day;
+            if (value.Hours > 0) return value.Hours + hour;
+            if (value.Minutes > 0) return value.Minutes + minute;
+            return value.Seconds + second;
+        }
     }
 }
