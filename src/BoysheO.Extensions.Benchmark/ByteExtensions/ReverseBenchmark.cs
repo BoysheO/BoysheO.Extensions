70fd2a76f75346315992235d5328b5bb56489e39
ADMINOR-JNBBMUB\admin <obx100@163.com>
2022-06-28 14:30:44 +0800

    benchmark reverse2

---
+++ src/BoysheO.Extensions.Benchmark/ByteExtensions/ReverseBenchmark.cs
@@ -0,0 +1,47 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BoysheO.Extensions;
+
+namespace Benchmark.ByteExtensions;
+
+[SimpleJob(RuntimeMoniker.Net60)]
+[RPlotExporter]
+[MemoryDiagnoser]
+public class ReverseBenchmark
+{
+    [Params(1, 12, 123, 1234, 12345, 123456, 1234567, 12345678, 123456789, 1234567890)]
+    public int Source;
+
+    [Benchmark]
+    public void LinqReverse()
+    {
+        var bytes = Prepare();
+        bytes = bytes.Reverse().ToArray();
+    }
+
+    [Benchmark]
+    public void SpanReverse()
+    {
+        var bytes = Prepare();
+        bytes.AsSpan().Reverse();
+    }
+
+    [Benchmark]
+    public void ArrayReverse()
+    {
+        var bytes = Prepare();
+        Array.Reverse(bytes);
+    }
+
+    [Benchmark]
+    public void MemoryExtensionsReverse()
+    {
+        var bytes = Prepare();
+        System.MemoryExtensions.Reverse(bytes.AsSpan());
+    }
+
+    private byte[] Prepare()
+    {
+        return Source.AsMemoryByteSpan().ToArray();
+    }
+}
