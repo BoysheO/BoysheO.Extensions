cc73d1da2cfe9c6fd713411e0c9978fa82250e76
ADMINOR-JNBBMUB\admin <obx100@163.com>
2022-06-28 14:30:18 +0800

    benchmark bytes reverse

--- src/BoysheO.Extensions.Benchmark/Program.cs
+++ src/BoysheO.Extensions.Benchmark/Program.cs
@@ -1,6 +1,7 @@
+using Benchmark.ByteExtensions;
 using Benchmark.Other;
 using Benchmark.StringExtensions;
 using BenchmarkDotNet.Running;
 
-var summary = BenchmarkRunner.Run<ForeachForSpanArray>();
+var summary = BenchmarkRunner.Run<ReverseBenchmark>();
 Console.WriteLine("hello world!");
