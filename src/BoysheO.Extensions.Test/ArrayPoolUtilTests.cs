f8af1fec5b170d8229e99256d8866e89c3a0fe7d
ADMINOR-JNBBMUB\admin <obx100@163.com>
2022-08-25 00:42:55 +0800

    update ArrayPoolUtil

---
+++ src/BoysheO.Extensions.Test/ArrayPoolUtilTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Linq;
+using Extensions;
+using NUnit.Framework;
+
+namespace BoysheO.Extensions.Test;
+
+public class ArrayPoolUtilTests
+{
+    private const int SizeMax = 1000000;
+    private const int TimesMax = 1000;
+
+    [TestCase]
+    public void RandomAdd()
+    {
+        var random = new Random();
+        var lst = new List<byte>();
+        var buff = ArrayPool<byte>.Shared.Rent(random.Next(1, SizeMax));
+        var elementCount = 0;
+        var times = random.NextInt64() % TimesMax;
+        Span<byte> b = stackalloc byte[1];
+        for (int i = 0; i < times; i++)
+        {
+            random.NextBytes(b);
+            elementCount = ArrayPoolUtil.Add(buff, elementCount, b[0], out buff);
+            lst.Add(b[0]);
+        }
+
+        Assert.AreEqual(lst.Count, elementCount);
+        Assert.AreEqual(lst.ToArray(), buff.Take(elementCount).ToArray());
+        ArrayPool<byte>.Shared.Return(buff);
+    }
+
+    [TestCase]
+    public void RandomAddRange()
+    {
+        var random = new Random();
+        var lst = new List<byte>();
+        var buff = ArrayPool<byte>.Shared.Rent(random.Next(1, SizeMax));
+        var elementCount = 0;
+        var times = random.NextInt64() % TimesMax;
+        byte[] b = new byte[223];
+        for (int i = 0; i < times; i++)
+        {
+            random.NextBytes(b);
+            elementCount = ArrayPoolUtil.AddRange(buff, elementCount, b, 0, b.Length, out buff);
+            lst.AddRange(b);
+        }
+
+        Assert.AreEqual(lst.Count, elementCount);
+        Assert.AreEqual(lst.ToArray(), buff.Take(elementCount).ToArray());
+        ArrayPool<byte>.Shared.Return(buff);
+    }
+}
--- src/BoysheO.Extensions/Util/ArrayPoolUtil.cs
+++ src/BoysheO.Extensions/Util/ArrayPoolUtil.cs
@@ -18,5 +18,52 @@ namespace Extensions
             ArrayPool<T>.Shared.Return(buff);
             return newBuff;
         }
+
+        /// <summary>
+        /// 给从ArrayPool中借出的Buff添加元素
+        /// ！不可以保留原buff的引用
+        /// </summary>
+        /// <param name="buff">原始buff</param>
+        /// <param name="elementCount">有效元素个数。须确保所有有效元素位于数组前端并连续</param>
+        /// <param name="addValue">需要添加的值</param>
+        /// <param name="outBuff">新的buff</param>
+        /// <returns>添加后的元素个数</returns>
+        public static int Add<T>(T[] buff, int elementCount, T addValue, out T[] outBuff)
+        {
+            if (elementCount >= buff.Length)
+            {
+                buff = Resize(buff, (int)(elementCount * 1.2));
+            }
+
+            buff[elementCount] = addValue;
+            outBuff = buff;
+            return elementCount + 1;
+        }
+
+        /// <summary>
+        /// 给从ArrayPool中借出的Buff添加元素
+        /// ！不可以保留原buff的引用
+        /// </summary>
+        /// <param name="buff">原始buff</param>
+        /// <param name="elementCount">有效元素个数。须确保所有有效元素位于数组前端并连续</param>
+        /// <param name="addValueAry">需要添加的值</param>
+        /// <param name="outBuff">新的buff</param>
+        /// <returns>添加后的元素个数</returns>
+        public static int AddRange<T>(T[] buff, int elementCount,
+            T[] addValueAry,
+            int addValueAryOffset,
+            int addValueAryCount,
+            out T[] outBuff)
+        {
+            if (elementCount + addValueAryCount > buff.Length)
+            {
+                buff = Resize(buff, elementCount + addValueAryCount);
+            }
+
+            var buffSpan = buff.AsSpan(elementCount, addValueAryCount);
+            addValueAry.AsSpan(addValueAryOffset,addValueAryCount).CopyTo(buffSpan);
+            outBuff = buff;
+            return elementCount + addValueAryCount;
+        }
     }
 }
