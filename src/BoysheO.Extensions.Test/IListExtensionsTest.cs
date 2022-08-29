44654014e959cf26ac3334e1ef2ee5badd593406
ADMINOR-JNBBMUB\admin <obx100@163.com>
2022-08-29 18:01:54 +0800

    qsortTest

---
+++ src/BoysheO.Extensions.Test/IListExtensionsTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using NUnit.Framework;
+
+namespace BoysheO.Extensions.Test;
+
+public class IListExtensionsTest
+{
+    [TestCase]
+    public void QSort()
+    {
+        var rand = new Random();
+        var list = new List<string>(rand.Next(0,40000));
+        var count = list.Capacity;
+        var compareList = new List<int>(count);
+        for (int i = 0; i < count; i++)
+        {
+            var v = rand.Next();
+            list.Add(v.ToString());
+            compareList.Add(v);
+        }
+        var clone = new List<string>(list);
+        list.QSort(compareList, 0, count-1);
+        
+        //valid
+        clone.Sort((a,b)=>int.Parse(a)-int.Parse(b));
+        Assert.AreEqual(list,clone);
+    }
+}
---
+++ src/BoysheO.Extensions/Extensions/IListExtensions.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+
+namespace BoysheO.Extensions
+{
+    public static class IListExtensions
+    {
+        //bug排序量达到40w后无法自己终止,end和start来回替换
+        public static void QSort<T>(this IList<T> list, IList<int> compareValue, int start, int end)
+        {
+            if (list.Count < 2) return;
+
+            void Swap(int idx1, int idx2)
+            {
+                (list[idx2], list[idx1]) = (list[idx1], list[idx2]);
+                (compareValue[idx2], compareValue[idx1]) = (compareValue[idx1], compareValue[idx2]);
+            }
+
+            bool flag = true;
+            while (end != start)
+            {
+                if (flag)
+                {
+                    int tempend = list.Count - 1;
+                    while (start < tempend)
+                    {
+                        if (compareValue[start] > compareValue[tempend]) //右侧找比自己小的数
+                        {
+                            Swap(start, tempend);
+                            flag = false;
+                            break;
+                        }
+                        else
+                        {
+                            tempend--;
+                            if (start == tempend)
+                            {
+                                start++;
+                                flag = false;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    int tempstart = 0;
+                    while (tempstart < end)
+                    {
+                        if (compareValue[tempstart] > compareValue[end]) //左侧找比自己大的数
+                        {
+                            Swap(tempstart, end);
+                            flag = true;
+                            break;
+                        }
+                        else
+                        {
+                            tempstart++;
+                            if (tempstart == end)
+                            {
+                                end--;
+                                flag = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
