48263519717b3695e0cb39ad26b69cecd3245564
boysheo <boysheo@outlook.com>
2022-11-01 18:03:36 +0800

    updaste

--- src/BoysheO.Extensions.Math/RandomUtil.cs
+++ src/BoysheO.Extensions.Math/RandomUtil.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Buffers;
 using System.Collections.Generic;
 
 namespace BoysheO.Extensions.Math
@@ -6,10 +7,22 @@ namespace BoysheO.Extensions.Math
     public static class RandomUtil
     {
         /// <summary>
-        /// 排列
+        /// 重新排列
         /// </summary>
-        public static void Permutation(Random random,Span<int> buffer,int minInclusive,int maxInclusive)
+        public static int[] Draw(Random random, ReadOnlySpan<int> buffer,
+            out int count)
         {
+            //一个数组随机交换多少次后可以视为随机？
+            // count = buffer.Length;
+            // var buf = ArrayPool<int>.Shared.Rent(count);
+            // var bufCount = 0;
+            // var burst = ArrayPool<int>.Shared.Rent(count);
+            // buffer.CopyTo(burst.AsSpan(0,count));
+            // for (int i = count - 1; i > 0; i++)
+            // {
+            //     var idx = random.Next(i);
+            //     
+            // }
             throw new NotImplementedException();
         }
     }
--- src/BoysheO.Extensions.Test/MathLibraryTest.cs
+++ src/BoysheO.Extensions.Test/MathLibraryTest.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
 using System.Linq;
 using BoysheO.Util;
 using NUnit.Framework;
@@ -17,7 +19,7 @@ public partial class MathLibraryTest
     {
         return MathLibrary.Factorial1(src).ToString();
     }
-    
+
     [TestCase(0, ExpectedResult = "1")]
     [TestCase(1, ExpectedResult = "1")]
     [TestCase(2, ExpectedResult = "2")]
@@ -27,21 +29,67 @@ public partial class MathLibraryTest
         return MathLibrary.Factorial2(src).ToString();
     }
 
-    [TestCase(1,1, ExpectedResult = "1")]
-    [TestCase(2,1, ExpectedResult = "2")]
-    [TestCase(3,2, ExpectedResult = "6")]
-    [TestCase(12,6, ExpectedResult = "665280")]
-    [TestCase(12,12, ExpectedResult = "479001600")]
+    [TestCase(1, 1, ExpectedResult = "1")]
+    [TestCase(2, 1, ExpectedResult = "2")]
+    [TestCase(3, 2, ExpectedResult = "6")]
+    [TestCase(12, 6, ExpectedResult = "665280")]
+    [TestCase(12, 12, ExpectedResult = "479001600")]
     public string Permutation(int n, int k)
     {
         return MathLibrary.Permutation(n, k).ToString();
     }
+
+    [TestCase(1, 1)]
+    [TestCase(2, 1)]
+    [TestCase(3, 2)]
+    [TestCase(12, 6)]
+    [TestCase(12, 12)]
+    public void DrawAsPooledArray(int poolSize, int count)
+    {
+        var res = MathLibrary.DrawAsPooledArray(poolSize, count);
+        Console.WriteLine(res.Take(count).Select(v=>v.ToString()).JoinAsOneString());
+        try
+        {
+            var set = new SortedSet<int>();
+            foreach (var i in res.AsSpan(0,count))
+            {
+                Assert.IsTrue(set.Add(i),"can not be same ele");
+            }
+        }
+        finally
+        {
+            ArrayPool<int>.Shared.Return(res);
+        }
+    }
+
+    [TestCase(1, 1)]
+    [TestCase(2, 1)]
+    [TestCase(3, 2)]
+    [TestCase(12, 6)]
+    [TestCase(12, 12)]
+    public void PermutationAsPooledArraySorted(int poolSize, int count)
+    {
+        var res = MathLibrary.DrawAsPooledArraySorted(poolSize, count);
+        Console.WriteLine(res.Take(count).Select(v=>v.ToString()).JoinAsOneString());
+        try
+        {
+            var set = new SortedSet<int>();
+            foreach (var i in res.AsSpan(0,count))
+            {
+                Assert.IsTrue(set.Add(i),"can not be same ele");
+            }
+        }
+        finally
+        {
+            ArrayPool<int>.Shared.Return(res);
+        }
+    }
     
-    [TestCase(1,1, ExpectedResult = "1")]
-    [TestCase(2,1, ExpectedResult = "2")]
-    [TestCase(3,2, ExpectedResult = "3")]
-    [TestCase(12,6, ExpectedResult = "924")]
-    [TestCase(12,12, ExpectedResult = "1")]
+    [TestCase(1, 1, ExpectedResult = "1")]
+    [TestCase(2, 1, ExpectedResult = "2")]
+    [TestCase(3, 2, ExpectedResult = "3")]
+    [TestCase(12, 6, ExpectedResult = "924")]
+    [TestCase(12, 12, ExpectedResult = "1")]
     public string Combination(int n, int k)
     {
         return MathLibrary.Combination(n, k).ToString();
