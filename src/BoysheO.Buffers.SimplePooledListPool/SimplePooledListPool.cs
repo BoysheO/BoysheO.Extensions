1c6606c28cf89916202c1aa1963e1f4aafdbe689
obx <946900978@qq.com>
2023-03-14 09:58:57 +0800

    simplePooledListPool

---
+++ src/BoysheO.Buffers.SimplePooledListPool/SimplePooledListPool.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Concurrent;
+using Collections.Pooled;
+
+namespace BoysheO.Buffers
+{
+    public class SimplePooledListPool<T>
+    {
+        public readonly SimplePooledListPool<T> Share = new();
+
+        private readonly WeakReference<ConcurrentBag<PooledList<T>>> _reference = new(new ConcurrentBag<PooledList<T>>());
+
+        private readonly object gate = new object();
+
+        public PooledList<T> Rent()
+        {
+            // ReSharper disable once InconsistentlySynchronizedField
+            if (!_reference.TryGetTarget(out var _pool))
+            {
+                lock (gate)
+                {
+                    if (!_reference.TryGetTarget(out _pool))
+                    {
+                        _pool = new ConcurrentBag<PooledList<T>>();
+                        _reference.SetTarget(_pool);
+                    }
+                }
+            }
+
+            if (_pool.TryTake(out var lst)) return lst;
+            lst = new PooledList<T>();
+            _pool.Add(lst);
+            return lst;
+        }
+
+        public void Return(PooledList<T> list)
+        {
+            // ReSharper disable once InconsistentlySynchronizedField
+            if (!_reference.TryGetTarget(out var _pool))
+            {
+                lock (gate)
+                {
+                    if (!_reference.TryGetTarget(out _pool))
+                    {
+                        _pool = new ConcurrentBag<PooledList<T>>();
+                        _reference.SetTarget(_pool);
+                    }
+                }
+            }
+
+            list.Clear();
+            list.Dispose();
+            _pool.Add(list);
+        }
+        
+    }
+}
