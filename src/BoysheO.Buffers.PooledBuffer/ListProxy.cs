35b3972d2baf086a4a738a90f292731eaed277e5
obx <946900978@qq.com>
2023-03-29 13:37:58 +0800

    change namespace

--- src/BoysheO.Buffers.PooledBuffer/ListProxy.cs
+++ src/BoysheO.Buffers.PooledBuffer/ListProxy.cs
@@ -2,7 +2,7 @@
 
 namespace BoysheO.Buffers
 {
-    public sealed class ListProxy<T>
+    internal sealed class ListProxy<T>
     {
         internal int Version = 1;
         internal readonly PooledList<T> List = new PooledList<T>();
