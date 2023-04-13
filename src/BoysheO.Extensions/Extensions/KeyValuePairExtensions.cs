d76f5fab1cd1f09038ca12baaa2461a87825bc58
obx <946900978@qq.com>
2023-04-13 13:29:22 +0800

    fix Deconstruct method implement incorrect

--- src/BoysheO.Extensions/Extensions/KeyValuePairExtensions.cs
+++ src/BoysheO.Extensions/Extensions/KeyValuePairExtensions.cs
@@ -4,9 +4,17 @@ namespace BoysheO.Extensions
 {
     public static class KeyValuePairExtensions
     {
-        public static (TK key, TV value) Deconstruct<TK, TV>(this KeyValuePair<TK, TV> kvp)
+        //这个函数实现解构函数的方式是错误的
+        //this method is incorrect
+        // public static (TK key, TV value) Deconstruct<TK, TV>(this KeyValuePair<TK, TV> kvp)
+        // {
+        //     return (kvp.Key, kvp.Value);
+        // }
+
+        public static void Deconstruct<TK, TV>(this KeyValuePair<TK, TV> kvp, out TK key, out TV value)
         {
-            return (kvp.Key, kvp.Value);
+            key = kvp.Key;
+            value = kvp.Value;
         }
     }
 }
