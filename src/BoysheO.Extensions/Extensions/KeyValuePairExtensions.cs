7880835177cefb51cfd3e67f4ab1e7518ddb012b
obx <946900978@qq.com>
2023-06-09 11:01:18 +0800

    update

--- src/BoysheO.Extensions/Extensions/KeyValuePairExtensions.cs
+++ src/BoysheO.Extensions/Extensions/KeyValuePairExtensions.cs
@@ -4,13 +4,6 @@ namespace BoysheO.Extensions
 {
     public static class KeyValuePairExtensions
     {
-        //这个函数实现解构函数的方式是错误的
-        //this method is incorrect
-        // public static (TK key, TV value) Deconstruct<TK, TV>(this KeyValuePair<TK, TV> kvp)
-        // {
-        //     return (kvp.Key, kvp.Value);
-        // }
-
         public static void Deconstruct<TK, TV>(this KeyValuePair<TK, TV> kvp, out TK key, out TV value)
         {
             key = kvp.Key;
