4b05938be01565eb259ef1b14d67eb29cb5ede76
boysheo <boysheo@outlook.com>
2024-05-02 21:29:21 +0800

    update Remove method

--- src/RefArrayPoolUtil/RefArrayPoolUtil/RefArrayPoolUtil.cs
+++ src/RefArrayPoolUtil/RefArrayPoolUtil/RefArrayPoolUtil.cs
@@ -118,7 +118,7 @@ namespace BoysheO.Extensions.Util
             buffCount = sizeNeed;
         }
 
-        public static bool Remove<T>(ref T[] buff, ref int buffCount, T obj, ArrayPool<T> pool = null)
+        public static bool Remove<T>(ref T[] buff, ref int buffCount, T obj)
         {
             var idx = Array.IndexOf(buff, obj, 0, buffCount);
             if (idx < 0) return false;
